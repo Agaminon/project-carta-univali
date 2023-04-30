c45c3fc29524b9bc74e91f71eb03f65908c855b7
Agaminon <131570562+Agaminon@users.noreply.github.com>
2023-04-30 18:30:28 +1200

    Add files via upload
    projeto carta

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h3>Brasil, 1/05/2023</h3>
+
+      <body>
+       <p>Olá Fabio, tudo bem?</p>  
+      
+      <p>Eu estou bem!<br /> <br />
+
+         Estou cursando Analise e Desenvolvimento de sistemas e é muito bom poder colocar em prática maus conhecimentos. <br />
+         Queria informar que estou gostando muito do trabalho e queria agradecer pela oportunidade de fazer parte da equipe <br /> na sua empresa. <br /> <br /></p>
+
+      <p>Até mais Sr. Fabio</p>
+
+      <h4>Att: Júlio César</h4>
+      </body>
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
